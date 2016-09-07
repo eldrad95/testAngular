c65288ffde986ee77af119b6c20e9d863cfc1304
Cyril Souied <CSouied@novipro.com>
2016-09-07 16:03:29 -0400

    fonctionnel

--- js/MyWebPart.js
+++ js/MyWebPart.js
@@ -1,6 +1,4 @@
 'use strict';
-
-
 var myWebPart = angular.module('MyWebPart',['ui.bootstrap']).directive('ngEnter', function() {
         return function(scope, element, attrs) {
             element.bind("keydown keypress", function(event) {
@@ -18,69 +16,82 @@ var myWebPart = angular.module('MyWebPart',['ui.bootstrap']).directive('ngEnter'
 myWebPart.factory('Values', function(){
 	var ob = {}
 	ob.eastValues = {};
-	ob.seteastValues = function(newobj) {
+	ob.setValues = function(newobj) {
 		ob.eastValues = newobj;
 
 	};
-	// this.getDatas = function() {
-	// 	return datas;
-	// }
-	ob.getEastValues = function() {
+	ob.getValues = function() {
 		return ob.eastValues;
 	}
 	return ob;
 	
 });
 
-
-
-// var infoBoxEast = 
-myWebPart.controller('infoBoxEast', function($scope, $interval, Values){
+myWebPart.controller('infoBox', function($scope, $interval, Values){
 	$scope.getData = function() {
-		$scope.Values = Values.getEastValues();
+		$scope.Values = Values.getValues();
 	}
-	$interval($scope.getData,1000);
-	$scope.city = 'Farnham';
-});
-myWebPart.controller('infoBoxWest', function($scope, $interval, Values){
-	$scope.getData = function() {
-		$scope.Values = Values.getEastValues();
-	}
-	$interval($scope.getData,1000);
-	$scope.city = 'Montreal';
+	$interval($scope.getData,500);
 });
 
-
-// var searchModule = 
 myWebPart.controller('searchController', function($scope,$http, Values){
 	$scope.headerTitle = 'Product Description...';
+	var Researchitems = [];
 	var data ;
 	$scope.search = function(Searchinput) {
-		var url = '../Fleetspec/php/connectdb-east.php';
-		var data = {'product': Searchinput };
-		var retourData;
-		$http({
-			url: url,
-			method : 'Post',
-			headers : {  'Content-type' : 'application/x-www-form-urlencoded'},
-			data: data,
-			transformRequest : serialize
-		}).success(function succesCallBack(response) {
-			Values.seteastValues(response[0]);
-			$scope.headerTitle = Values.eastValues.Descrip_east;
-		},function errorCallBack(response){
-			alert(response.data);
-		})
+		if (searchInput != '')
+		{
+			if (Researchitems.indexOf(Searchinput) == -1)
+			{
+				Researchitems.push(Searchinput);
+			}
+			var url = '../AngularService/WebServiceAngular.FleetSpecService.svc/FleetSpecService/?product=' 
+			+ Searchinput;
+			var retourData;
+			$http.get(url)
+			.success(function succesCallBack(response) {
+				Values.setValues(response);
+				$scope.headerTitle = Values.getValues()[0].descrip;
+			},function errorCallBack(response){
+				alert(response.data);
+			})
+		}
 	}
+
+	$scope.previous = function() {
+		if($scope.searchInput !== '' && Researchitems.indexOf($scope.searchInput) > 0)
+		{
+			$scope.searchInput = Researchitems[Researchitems.indexOf($scope.searchInput) -1] ;
+		}
+		else if (Researchitems.indexOf($scope.searchInput) == 0)
+		{
+			return;
+		}
+		else {
+			$scope.searchInput = Researchitems[Researchitems.length -1 ];
+		}
+	}
+
+	$scope.next = function() {
+		if($scope.searchInput !== '' && Researchitems.indexOf($scope.searchInput) < Researchitems.length -1)
+		{
+			$scope.searchInput = Researchitems[Researchitems.indexOf($scope.searchInput) +1] ;
+		}
+		else {
+			$scope.searchInput = '';
+		}
+	}
+	
 });
 
+//transform json request in url request for services. 
+// var	serialize = function(obj) {
+//   var str = [];
+//   for(var p in obj)
+//     if (obj.hasOwnProperty(p)) {
+//       str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+//     }
+//   console.log(str.join("&"));
+//   return str.join("&");
 
-var	serialize = function(obj) {
-  var str = [];
-  for(var p in obj)
-    if (obj.hasOwnProperty(p)) {
-      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-    }
-  return str.join("&");
-}
-
+// }
